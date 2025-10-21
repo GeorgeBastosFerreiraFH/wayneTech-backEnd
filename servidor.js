6c4ebe0b4bc71a255d9e649810c31a706d1e43e6
George Bastos <114554305+GeorgeBastosFerreiraFH@users.noreply.github.com>
2025-10-21 19:20:48 -0300

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -2,7 +2,7 @@ import express from "express"
 import cors from "cors"
 import pkg from "pg"
 import jwt from "jsonwebtoken"
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 
 const { Pool } = pkg
 
